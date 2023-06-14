ef68d6622bf5a199b73a7e1de8b741c956f9a7c0
fable <princessrave660@hotmail.com>
2023-06-13 21:14:40 -0400

    OCR 1/2

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
 
-    client.application?.commands.set([]);
+    client.commands.set(command.data.name, command);
 
 }
 
