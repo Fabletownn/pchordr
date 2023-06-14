685b5ab78b49fbd3c5dca962aedb2256b227a4ce
fable <princessrave660@hotmail.com>
2023-06-13 21:20:11 -0400

    OCR 2/3 2

--- deploy-commands.js
+++ deploy-commands.js
@@ -24,7 +24,7 @@ const rest = new REST({
 
 }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), {
+rest.put(Routes.applicationCommands(process.env.CLIENTID), {
 
     body: []
 
