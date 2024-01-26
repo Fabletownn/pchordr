791120898f7339545d948f11fdde220037e99520
fable <princessrave660@hotmail.com>
2024-01-26 13:15:25 -0500

    potential giveaway fix

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -148,22 +148,17 @@ module.exports = async (Discord, client, message) => {
                         if (data.giveawayWinnerRole !== null) {
                             await message.guild.roles.cache.find(role => role.id === data.giveawayWinnerRole).members.forEach(async (winner) => {
                                 await winner.roles.remove(data.giveawayWinnerRole);
-                                await message.guild.channels.cache.get(data.modChat).send({ content: `Removed <@&${data.giveawayWinnerRole}> role from <@${winner.user.id}> (${roleCounter} member(s))..` });
+                                await message.guild.channels.cache.get(data.modChat).send({ content: `Removed <@&${data.giveawayWinnerRole}> role from <@${winner.user.id}>..` });
                             });
-                        }
-
-                        // Add winner role for every mentioned user in giveaways
-                        if (data.giveawayWinnerRole !== null) {
-                            let winnerCounter = 0;
 
                             await message.mentions.users.forEach(async (winner) => {
                                 await message.guild.members.cache.get(winner.id).roles.add(data.giveawayWinnerRole);
                                 winnerCounter++;
-                                await message.guild.channels.cache.get(data.modChat).send({ content: `Added <@&${data.giveawayWinnerRole}> role to <@${winner.id}> (${winnerCounter} member(s))..` });
+                                await message.guild.channels.cache.get(data.modChat).send({ content: `Added <@&${data.giveawayWinnerRole}> role to <@${winner.id}>..` });
                             });
                         }
 
-                        await message.guild.channels.cache.get(data.modChat).send({ content: `_ _\nDone! <:bITFGG:1022548636481114172>` });
+                        setTimeout(async() => await message.guild.channels.cache.get(data.modChat).send({ content: `_ _\nDone! <:bITFGG:1022548636481114172>` }), 1000);
                     }
                 }
             }
