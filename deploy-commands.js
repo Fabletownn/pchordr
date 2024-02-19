979fe26829cb9ed954197c7c0ce80507ac3a1626
fable <princessrave660@hotmail.com>
2024-02-18 21:35:52 -0500

    adding appeal server

--- deploy-commands.js
+++ deploy-commands.js
@@ -24,7 +24,7 @@ const rest = new REST({
 
 }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), {
+rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, [process.env.GUILDID, '685876599199236173']), {
 
     body: commands
 
