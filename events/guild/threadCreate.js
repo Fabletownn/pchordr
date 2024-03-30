79931bf12b4a1e9721a74f8f8fea4765de08da54
fable <princessrave660@hotmail.com>
2024-03-30 13:29:30 -0400

    adding post fetch

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -7,22 +7,21 @@ module.exports = async (Discord, client, thread, newlyCreated) => {
         if (err) return console.log(err);
         if (!data) return;
 
-        console.log(thread.messages);
-
         if (newlyCreated) {
             if ((data.artChat !== null) && (data.gpChat !== null) && (data.modRole !== null) && (data.artdelete !== null)) {
+                if (data.artdelete === false) return;
 
-                if ((thread.parentId === data.artChat || thread.parentId === data.gpChat) && (thread.messages.first().attachments.size <= 0) && (!thread.messages.first().member.roles.cache.has(data.modRole)) && (!thread.messages.first().content.includes('https://')) && (!thread.messages.first().content.includes('www.'))) {
+                thread.fetchStarterMessage().then((post) => {
+                    console.log(post);
 
-                    if (data.artdelete === true) {
-
+                    if ((thread.parentId === data.artChat || thread.parentId === data.gpChat) && (post.attachments.size <= 0) && (!post.member.roles.cache.has(data.modRole)) && (!post.content.includes('https://')) && (!post.content.includes('www.'))) {
                         if (thread.author.bot) return;
 
                         setTimeout(() => thread.send(`<@${thread.ownerId}>: Make sure your post has an image or media link attached to it! This post will automatically delete in 5 seconds. <a:bITFLeave:1063266447251492935>`), 1500);
 
                         setTimeout(() => thread.delete(), 5500);
                     }
-                }
+                });
             }
         }
     });
