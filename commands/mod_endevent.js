4d81678b763d19aa6b049b4ea17921010e730766
fable <princessrave660@hotmail.com>
2024-04-06 18:56:39 -0400

    fixing and improving event launch/end commands

--- commands/mod_endevent.js
+++ commands/mod_endevent.js
@@ -6,60 +6,124 @@ const {
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('event-end')
-        .setDescription('Ends all ongoing Server Events, if any')
+        .setDescription('Locks ongoing event channels if one has ended')
         .setDMPermission(false)
-        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
-
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addStringOption((option) =>
+            option.setName('event')
+                .setDescription('What ongoing event should be closed down?')
+                .setRequired(true)
+                .addChoices({
+                    name: 'Fortnite Custom',
+                    value: 'fortnite_custom'
+                }, {
+                    name: 'Server Event',
+                    value: 'server_event'
+                }, {
+                    name: 'Discord Stream',
+                    value: 'discord_stream'
+                }, {
+                    name: 'Discord Movie',
+                    value: 'discord_movie'
+                })
+        ),
     async execute(interaction) {
+        const to_end = interaction.options.get('event').value;
 
-        const serverEvents = interaction.guild.channels.cache.get('815790709646163968');
-        const eventsChat = interaction.guild.channels.cache.get('973751203756400660');
-        const codesChan = interaction.guild.channels.cache.get('771394751316099133');
+        //////////// Channels
+        const serverEventsVC = interaction.guild.channels.cache.get('815790709646163968');
+        const movieEventsVC = interaction.guild.channels.cache.get('1160656322103087175');
+        const streamChat = interaction.guild.channels.cache.get('973751203756400660');
+        const customsCodeChat = interaction.guild.channels.cache.get('1198018037538881586');
 
-        await serverEvents.permissionOverwrites.edit(interaction.guild.id, {
-            
-            ViewChannel: false,
+        //////////// Role IDs
+        const serverEventsRole = '731212776936308777';
+        const discordStreamsRole = '816705412451008542';
+        const fortniteCustomsRole = '731217324765479053';
 
-        }, `@${interaction.user.username} has requested the event to end`);
+        switch (to_end) {
+            case "discord_stream": // (Discord Streams role) Disable permissions for Server Events and Stream Chat
+                await serverEventsVC.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: false,
+                    Connect: false
+                });
 
-        await eventsChat.permissionOverwrites.edit(interaction.guild.id, {
+                await streamChat.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: false,
+                    SendMessages: false
+                });
 
-            ViewChannel: false,
+                if (serverEventsVC.members.size > 0) {
+                    serverEventsVC.members.each(async (member) => {
+                        await member.voice.disconnect();
+                    });
+                }
 
-        }, `@${interaction.user.username} has requested the event to end`);
+                await interaction.reply({ content: `Closed up the <#${serverEventsVC}> (all members disconnected) and <#${streamChat}> channels for the <@&${discordStreamsRole}> role. Hope you had fun! <:bITFWave:1022548691988512778>` });
+                break;
+            case "server_event": // (Server Events role) Disable permissions for Server Events and Stream Chat
+                await serverEventsVC.permissionOverwrites.edit(serverEventsRole, {
+                    ViewChannel: false,
+                    Connect: false
+                });
 
-        await serverEvents.permissionOverwrites.edit('731212776936308777', {
+                await streamChat.permissionOverwrites.edit(serverEventsRole, {
+                    ViewChannel: false,
+                    SendMessages: false
+                });
 
-            ViewChannel: false,
+                if (serverEventsVC.members.size > 0) {
+                    serverEventsVC.members.each(async (member) => {
+                        await member.voice.disconnect();
+                    });
+                }
 
-        }, `@${interaction.user.username} has requested the event to end`);
+                await interaction.reply({ content: `Closed up the <#${serverEventsVC}> (all members disconnected) and <#${streamChat}> channels for the <@&${serverEventsRole}> role. Hope you had fun! <:bITFWave:1022548691988512778>` });
+                break;
+            case "fortnite_custom": // (Fortnite Customs role) Disable permissions for Server Events, Customs Chat, and Stream Chat
+                await serverEventsVC.permissionOverwrites.edit(fortniteCustomsRole, {
+                    ViewChannel: false,
+                    Connect: false
+                });
 
-        await eventsChat.permissionOverwrites.edit('731212776936308777', {
+                await customsCodeChat.permissionOverwrites.edit(fortniteCustomsRole, {
+                    ViewChannel: false
+                });
 
-            ViewChannel: false,
+                await streamChat.permissionOverwrites.edit(fortniteCustomsRole, {
+                    ViewChannel: false,
+                    SendMessages: false
+                });
 
-        }, `@${interaction.user.username} has requested the event to end`);
+                if (serverEventsVC.members.size > 0) {
+                    serverEventsVC.members.each(async (member) => {
+                        await member.voice.disconnect();
+                    });
+                }
 
-        await serverEvents.permissionOverwrites.edit('731217324765479053', {
+                await interaction.reply({ content: `Closed up the <#${serverEventsVC}> (all members disconnected), <#${customsCodeChat}>, and <#${streamChat}> channels for the <@&${fortniteCustomsRole}> role. Hope you had fun! <:bITFWave:1022548691988512778>` });
+                break;
+            case "discord_movie": // (Discord Streams role) Disable permissions for Discord Movies and Stream Chat
+                await movieEventsVC.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: false,
+                    Connect: false
+                });
 
-            ViewChannel: false,
+                await streamChat.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: false,
+                    SendMessages: false
+                });
 
-        }, `@${interaction.user.username} has requested the event to end`);
+                if (movieEventsVC.members.size > 0) {
+                    movieEventsVC.members.each(async (member) => {
+                        await member.voice.disconnect();
+                    });
+                }
 
-        await eventsChat.permissionOverwrites.edit('731217324765479053', {
-
-            ViewChannel: false,
-
-        }, `@${interaction.user.username} has requested the event to end`);
-
-        await codesChan.permissionOverwrites.edit('731217324765479053', {
-
-            ViewChannel: false,
-
-        }, `@${interaction.user.username} has requested the event to end`);
-
-        await interaction.reply(`Closed down all Server Event voice & text channels for all members and roles. <:bITFVictory:1063265610303295619>`);
-
+                await interaction.reply({ content: `Closed up the <#${movieEventsVC}> (all members disconnected) and <#${streamChat}> channels for the <@&${discordStreamsRole}> role. Hope you had fun! <:bITFWave:1022548691988512778>` });
+                break;
+            default:
+                break;
+        }
     },
-
 };
---
+++ commands/mod_launchevent.js
@@ -0,0 +1,105 @@
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits
+} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('event-launch')
+        .setDescription('Unlocks beginning event channels if one has started')
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addStringOption((option) =>
+            option.setName('event')
+                .setDescription('What event should be started?')
+                .setRequired(true)
+                .addChoices({
+                    name: 'Fortnite Custom',
+                    value: 'fortnite_custom'
+                }, {
+                    name: 'Server Event',
+                    value: 'server_event'
+                }, {
+                    name: 'Discord Stream',
+                    value: 'discord_stream'
+                }, {
+                    name: 'Discord Movie',
+                    value: 'discord_movie'
+                })
+        ),
+    async execute(interaction) {
+        const to_launch = interaction.options.get('event').value;
+
+        //////////// Channels
+        const serverEventsVC = interaction.guild.channels.cache.get('815790709646163968');
+        const movieEventsVC = interaction.guild.channels.cache.get('1160656322103087175');
+        const streamChat = interaction.guild.channels.cache.get('973751203756400660');
+        const customsCodeChat = interaction.guild.channels.cache.get('1198018037538881586');
+
+        //////////// Role IDs
+        const serverEventsRole = '731212776936308777';
+        const discordStreamsRole = '816705412451008542';
+        const fortniteCustomsRole = '731217324765479053';
+
+        switch (to_launch) {
+            case "discord_stream": // (Discord Streams role) Enable permissions for Server Events and Stream Chat
+                await serverEventsVC.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: true,
+                    Connect: true
+                });
+
+                await streamChat.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: true,
+                    SendMessages: true
+                });
+
+                await interaction.reply({ content: `Unlocked the <#${serverEventsVC}> and <#${streamChat}> channels for the <@&${discordStreamsRole}> role. <:bITFVictory:1063265610303295619>` });
+                break;
+            case "server_event": // (Server Events role) Enable permissions for Server Events and Stream Chat
+                await serverEventsVC.permissionOverwrites.edit(serverEventsRole, {
+                    ViewChannel: true,
+                    Connect: true
+                });
+
+                await streamChat.permissionOverwrites.edit(serverEventsRole, {
+                    ViewChannel: true,
+                    SendMessages: true
+                });
+
+                await interaction.reply({ content: `Unlocked the <#${serverEventsVC}> and <#${streamChat}> channels for the <@&${serverEventsRole}> role. <:bITFVictory:1063265610303295619>` });
+                break;
+            case "fortnite_custom": // (Fortnite Customs role) Enable permissions for Server Events, Customs Chat, and Stream Chat
+                await serverEventsVC.permissionOverwrites.edit(fortniteCustomsRole, {
+                    ViewChannel: true,
+                    Connect: true
+                });
+
+                await customsCodeChat.permissionOverwrites.edit(fortniteCustomsRole, {
+                    ViewChannel: true
+                });
+
+                await streamChat.permissionOverwrites.edit(fortniteCustomsRole, {
+                    ViewChannel: true,
+                    SendMessages: true
+                });
+
+                await interaction.reply({ content: `Unlocked the <#${serverEventsVC}>, <#${customsCodeChat}>, and <#${streamChat}> channels for the <@&${fortniteCustomsRole}> role. <:bITFVictory:1063265610303295619>` });
+                break;
+            case "discord_movie": // (Discord Streams role) Enable permissions for Discord Movies and Stream Chat
+                await movieEventsVC.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: true,
+                    Connect: true
+                });
+
+                await streamChat.permissionOverwrites.edit(discordStreamsRole, {
+                    ViewChannel: true,
+                    SendMessages: true
+                });
+
+                await interaction.reply({ content: `Unlocked the <#${movieEventsVC}> and <#${streamChat}> channels for the <@&${discordStreamsRole}> role. <:bITFVictory:1063265610303295619>` });
+                break;
+            default:
+                break;
+        }
+    },
+};
