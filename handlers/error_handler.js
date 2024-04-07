5b43b9db4f102cbba00869f5c8e4915512ff08de
fable <princessrave660@hotmail.com>
2024-04-07 18:15:34 -0400

    forgot var

--- handlers/error_handler.js
+++ handlers/error_handler.js
@@ -1,25 +1,27 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) => {
-    process.on('unhandledRejection', (reason, promise) => {
-        console.log(reason, promise);
-        return console.log(`An unhandled rejection error came through.\n${reason}\n${promise}`);
-    });
+    const load_dir = (dirs) => {
+        process.on('unhandledRejection', (reason, promise) => {
+            console.log(reason, promise);
+            return console.log(`An unhandled rejection error came through.\n${reason}\n${promise}`);
+        });
 
-    process.on('uncaughtException', (reason, promise) => {
-        console.log(reason, promise);
-        return console.log(`An uncaught exception error came through.\n${reason}\n${promise}`);
-    });
+        process.on('uncaughtException', (reason, promise) => {
+            console.log(reason, promise);
+            return console.log(`An uncaught exception error came through.\n${reason}\n${promise}`);
+        });
 
-    process.on('uncaughtExceptionMonitor', (reason, promise) => {
-        console.log(reason, promise);
-        return console.log(`An uncaught exception error came through via Monitor.\n${reason}\n${promise}`);
-    });
+        process.on('uncaughtExceptionMonitor', (reason, promise) => {
+            console.log(reason, promise);
+            return console.log(`An uncaught exception error came through via Monitor.\n${reason}\n${promise}`);
+        });
 
-    process.on('rejectionHandled', (reason, promise) => {
-        console.log(reason, promise);
-        return console.log(`A promise rejection error came through.\n${reason}\n${promise}`);
-    });
+        process.on('rejectionHandled', (reason, promise) => {
+            console.log(reason, promise);
+            return console.log(`A promise rejection error came through.\n${reason}\n${promise}`);
+        });
+    }
 
     ['guild'].forEach(e => load_dir(e));
 }
